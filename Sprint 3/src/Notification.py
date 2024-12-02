880669e226d828c67555865299dc8873edaea2a4
Trevor Corcoran <supershadowplay@gmail.com>
2024-12-02 11:01:33 -0500

    Functioning notification picture and sound

--- Sprint 3/src/Notification.py
+++ Sprint 3/src/Notification.py
@@ -3,19 +3,21 @@ Name: Notification.py
 Description: Holds a wrapper function that sends the medication reminder notification.
 """
 
-from windows_toasts import WindowsToaster, ToastDisplayImage, Toast
+from windows_toasts import WindowsToaster, ToastDisplayImage, ToastAudio, AudioSource, Toast
+from os import path
 
-ICO_PATH = "../assets/medical_icon.png"
+ICO_PATH = path.abspath("../assets/medical_icon.png")
 
 
 def send(medication_name: str, description_text: str) -> None:
     """Takes input fields and sends a notification to remind the user to take medication"""
-    toaster = WindowsToaster(f"Medication Reminder: {medication_name}")  # Sets title/initializes notification server
+    toaster = WindowsToaster("Medication Reminder")  # Sets title/initializes notification server
 
     # Configure notification body
     toast_body = Toast()
-    toast_body.text_fields = [description_text]  # Sets description text
-    toast_body.AddImage(ToastDisplayImage.fromPath(ICO_PATH))
+    toast_body.text_fields = [f"{medication_name}\n{description_text}"]  # Sets description text
+    toast_body.AddImage(ToastDisplayImage.fromPath(ICO_PATH))  # Display app logo
+    toast_body.audio = ToastAudio(AudioSource.IM, looping=True)  # Gentle alarm sound until dismissed
 
     # Display notification
     toaster.show_toast(toast_body)
